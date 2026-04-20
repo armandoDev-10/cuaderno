0b599d5a6a0c894700de8468d212b92094d992e1
armandoDev-10 <123055526+armandoDev-10@users.noreply.github.com>
2026-04-20 17:20:23 -0600

    Add files via upload

---
+++ apuntes.js
@@ -0,0 +1,12 @@
+const apuntesData = [
+    {
+        "materia":"Proyecto de Investigación",
+        "fecha":"2026-04-17",
+        "link":"https://good-file-e26.notion.site/Apuntes-de-clase-34856392eb578097a952c7ba7589d2ef?pvs=143"
+    },
+    {
+        "materia":"Proyecto",
+        "fecha":"2026-04-17",
+        "link":"https://good-file-e26.notion.site/Apuntes-de-clase-34856392eb578097a952c7ba7589d2ef?pvs=143"
+    }
+]
---
+++ sitios.js
@@ -0,0 +1,7 @@
+const sitiosData = [
+    {
+        "nombre":"Investigador PRO",
+        "url":"https://armandodev-10.github.io/gestor_invest/",
+        "descripcion":"Herramienta para centralizar recursos de investigación, notas de lectura y gestión de proyectos académicos."
+    }
+]
